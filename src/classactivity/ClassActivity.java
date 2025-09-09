b0742e680344476140bc0d4eb0eb202fbafd83ce
David <sander.david2005@gmail.com>
2025-09-09 09:53:41 +0200

    Initial commit

---
+++ src/classactivity/ClassActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classactivity;
+
+/**
+ *
+ * @author David
+ */
+public class ClassActivity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
